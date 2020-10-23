baa4a917a19adb8994297909112747bf1dedb4b5
Lucianoquiroga <quiroga_180@hotmail.com>
2020-10-22 22:21:16 -0300

    update-images

--- db.sql
+++ db.sql
@@ -4,8 +4,8 @@ create table message(
     email varchar(100) not null,
     phone varchar(30) not null,
     city varchar(50) not null,
-    date_start datetime not null,
-    date_end datetime not null,
+    date_start date not null,
+    date_end date not null,
     message_user varchar(255) null,
     PRIMARY KEY (id)
 )ENGINE = InnoDB
